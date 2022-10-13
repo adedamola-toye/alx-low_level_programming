0f36b11f3b52faac2e63dea27c57e418cb43f2f0
adedamola-toye <toye.adedamola@gmail.com>
2022-10-13 13:17:42 +0000

    Edit the text

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry
  * Return: Always 0;
-*/
+ */
 int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
-	printf("Size of a char: %i byte(S)\n", sizeof(int));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("SIze of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
