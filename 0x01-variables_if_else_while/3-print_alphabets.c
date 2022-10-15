13128607420ce7590e4203cfd274330cb82eff7b
adedamola-toye <toye.adedamola@gmail.com>
2022-10-14 21:11:05 -0700

    Print alphabets in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lower and uppercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lu;
+
+	for (lu = 'a'; lu <= 'z'; lu++)
+	{
+		putchar(lu);
+	}
+
+	for (lu = 'A'; lu <= 'Z'; lu++)
+	{
+		putchar(lu);
+	}
+	putchar('\n');
+	return (0);
+}
