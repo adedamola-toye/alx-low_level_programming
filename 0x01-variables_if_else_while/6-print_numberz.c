021065588d890c697a717d51c6b03a2bf75db7eb
adedamola-toye <toye.adedamola@gmail.com>
2022-10-14 21:59:26 -0700

    Print last didgits

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints all single giits of base 10
- * starting from 0
+ * main - print single didgits starting from 0
+ * in base 10
  * Return: Always 0
  */
 int main(void)
@@ -10,9 +10,9 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-	{
 		putchar((num % 10) + '0');
-	}
-	putchar("\n");
+
+	putchar('\n');
+
 	return (0);
 }
